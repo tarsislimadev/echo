f2b8069cbf12879e18dfa180f3231fd8df058578
Tarsis <br.tmvdl@gmail.com>
2024-03-03 22:01:05 -0300

    Updating app files

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const http = require('http')
 const { Server } = require('socket.io')
+
 const { PORT } = require('./config.js')
+
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 
 app.use(express.static('public'))
 
-// io.on('connection', (socket) => console.log('a user connected', socket.id))
+io.on('connection', (socket) => {
+  console.log(`connected ${socket.id}`)
 
-io.on('message', (message) => io.emit(message))
+  socket.on('message', (message) => io.emit('message', message))
+})
 
 server.listen(PORT, () => console.log(`listenning on ${PORT}`))
--- src/api/public/index.js
+++ src/api/public/index.js
@@ -1,9 +1,19 @@
 import { HTML } from '@brtmvdl/frontend'
 
 export class Page extends HTML {
+  state = { socket: io(), }
+
   onCreate() {
     super.onCreate()
-    //
     this.setText('page')
+    this.state.socket.on('message', console.log)
+    this.append(this.getButton())
+  }
+
+  getButton() {
+    const html = new HTML()
+    html.setText('click here')
+    html.on('click', () => this.state.socket.emit('message', Date.now()))
+    return html
   }
 }
